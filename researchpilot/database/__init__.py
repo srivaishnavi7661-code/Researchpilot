5a98ba07f918a1ddd1f4bc99ded46a8f76307dba
M. Sri Vaishnavi <srivaishnavi7661@gmail.com>
2026-03-10 11:10:23 +0530

    Delete researchpilot/database directory

--- researchpilot/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import Paper, Collection, Tag, ResearchSession, SearchHistory, Base
-from .manager import DatabaseManager
-
-__all__ = ["Paper", "Collection", "Tag", "ResearchSession", "SearchHistory", "Base", "DatabaseManager"]
